324a947e63fb20e307a9ce7e8f36e1162abe09b6
CodeZavian <nomiemerge9901@gmail.com>
2024-05-26 13:53:55 +0500

    change

--- JS/EX_1.js
+++ JS/EX_1.js
@@ -1,8 +1,46 @@
-let random = Math.random();
+/* Create a faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. It takes two numbers as input from the user
+2. It perfoms wrong operations as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+It performs wrong operation 10% of the times
+
+*/
+
+let random = Math.random()
+console.log(random)
+let a = prompt("Enter first number")
+let c = prompt("Enter operation")
+let b = prompt("Enter second number")
 
 
-// input 
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
 
-let n1 = prompt("enter 1st number")
-let operation = prompt("enter + || - || * || /")
-let n2 = prompt("enterr 2nd number")
+ // 90% it give correct and 10 % it will give false result
+ 
+if (random > 0.1) { 
+    // Perform correct calculation
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+
+    
+}
+
+else {
+    // Perform wrong calculation
+    c = obj[c]
+    
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
+
+}
