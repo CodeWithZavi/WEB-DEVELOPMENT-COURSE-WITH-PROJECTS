6984878ff4a5d982083b8f63b983001fc4c06de7
CodeZavian <nomiemerge9901@gmail.com>
2024-05-26 21:48:37 +0500

    //

---
+++ JS/String.js
@@ -0,0 +1,29 @@
+console.log("This is strings tutorial")
+let a = "Noman";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length)
+
+let real_name = "Sardar"
+let friend = "Noman"
+
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "ZaviAN"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+ // we cant change string cuz string is immuteable 
+console.log(b.replace("Za", "7&"))
+console.log(b.concat(" "+a, " Aishwariya ", " Rahul ", " Priya"))
+
+console.log(b)
