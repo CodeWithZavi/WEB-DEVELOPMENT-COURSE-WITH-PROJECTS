531740efaf5c55d17d418419ffa2aa2867651009
CodeZavian <nomiemerge9901@gmail.com>
2024-09-20 09:33:51 +0500

    //

---
+++ JS/noman.js
@@ -0,0 +1,14 @@
+class student {
+
+    constructor(id, name, cgpa) {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+}
+function PrintName() {
+    const student = []
+    student = prompt("enter students name ")
+
+
+}
